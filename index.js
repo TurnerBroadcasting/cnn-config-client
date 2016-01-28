daf40aa1cd1456a6ac5219986edde4b79b003eaa
A.D. Slaton <ad.slaton@turner.com>
2016-01-28 16:59:17 -0500

    Allowing the client to set the env vars

--- index.js
+++ index.js
@@ -1,64 +1,70 @@
 'use strict';
 
-const util = require('util'), 
+const util = require('util'),
     fetch = require('node-fetch'),
     hosts = {
         dev: 'http://cnn-config-server.dev.services.ec2.dmtio.net:5000',
         prod: 'http://cnn-config-server.prod.services.ec2.dmtio.net:5000'
     };
-    
+
 function testOptions (options) {
     return typeof options === 'undefined' || typeof options !== 'object' ? {} : options;
 }
 
+function setEnv (json) {
+    let prop;
+
+    for (prop in json) {
+        if (json.hasOwnProperty(prop)) {
+            process.env[prop] = json[prop];
+        }
+    }
+}
+
 function register (options) {
     options = testOptions(options);
-    
+
     let url = util.format('%s/%s', hosts[options.environment], 'register');
-    
+
     fetch(url, {method: 'POST', body: options.data})
         .then(function (res) {
             return res.json();
         })
         .then(function (json) {
-            console.log(json);
-        })
+            setEnv(json);
+        });
 }
 
 function update (options) {
     options = testOptions(options);
-    
+
     let url = util.format('%s/%s', hosts[options.environment], 'update');
-    
+
     fetch(url, {method: 'POST', body: options.data})
         .then(function (res) {
             return res.json();
         })
         .then(function (json) {
-            console.log(json);
-        })
+            setEnv(json);
+        });
 }
 
 function getConfig (options) {
     options = testOptions(options);
-    
-    console.log('%s/%s/%s/%s', hosts[options.environment], options.product, options.environment, options.token);
-    
+
     let url = util.format('%s/%s/%s/%s', hosts[options.environment], options.product, options.environment, options.token);
-    
-    console.log(url);
-    
+
     fetch(url)
         .then(function (res) {
             return res.json();
         })
         .then(function (json) {
-            console.log(json);
-        })
+            setEnv(json);
+        });
 }
 
 module.exports = {
     register: register,
     update: update,
     getConfig: getConfig
-}
+};
